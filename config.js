b7721d560d5293e9d3ff823c3d265eb68810e444
Jackson Huang <jahuang@jahuang-mac.tsi.lan>
2016-02-04 18:24:57 -0800

    wdc-setup

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'HOSTPATH': 'http://localhost',
+  'PORT': 8080,
+  'SURVEY_MONKEY_CLIENT_ID': 'sm_jhuang8598',
+  'SURVEY_MONKEY_CLIENT_SECRET': 'xsWuZTPWZvyAj8txmGmtqcFyFP8kTHhH',
+  'SURVEY_MONKEY_API_KEY': '5c4x5n79vbfqr95pc2x5u87y',
+  'SURVEY_MONKEY_REDIRECT_URL': 'http://localhost:8080/redirect'
+};
