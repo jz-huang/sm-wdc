c295183c19956742874eb0e3aeaefb4161aa9288
Jackson Huang <jahuang@jahuang-mac.tsi.lan>
2016-02-09 19:33:53 -0800

    only parsing left to do

--- public/javascripts/smWDC.js
+++ public/javascripts/smWDC.js
@@ -1,4 +1,4 @@
-
+(function(){
 
 var config = {
 	redirect_uri: 'http://localhost:8080/redirect',
@@ -35,15 +35,18 @@ $(document).ready(function() {
 	
 	$("#getDataButton").click(function() {
 
-	//	tableau.connectionName = "Survey Monkey Data";
-	// 	tableau.submit();
+
 		var survey_id = $('#surveys').val().trim();
 		if (survey_id == '-1') {
 			alert('Error: no surveys found on this account'); 
 		} else {
-			_getColumnHeaders(survey_id, accessToken); 
-		}
+			//_getRespondentList(survey_id, accessToken);
 
+			tableau.connectionName = 'Survey Monkey Data' + $('#surveys option:selected').text();
+			tableau.connectionData = survey_id; 
+			tableau.alwaysShowAuthUI = true;
+			tableau.submit();
+		}
 	});
 
 	var hasAuth = accessToken && accessToken.length > 0;
@@ -111,7 +114,7 @@ function request_survey_list(accessToken){
 function parse_survey_list(surveys){
 	surveys.sort(date_compare); //sort by date modified
 
-	console.log(surveys);
+	//console.log(surveys);
 	if(surveys.length == 0) {
 		$('#surveys').append('<option value=-1>No Surveys Found </option>'); 
 		return; 
@@ -123,13 +126,13 @@ function parse_survey_list(surveys){
 };
 
 function date_compare(survey1, survey2){
-	console.log(survey1.date_modified);
+	//console.log(survey1.date_modified);
 	var date1 = new Date(survey1.date_modified);
 	var date2 = new Date(survey2.date_modified); 
 	return -(date1-date2); 
 }
 
-var data;
+var data, h, f;
 function _getColumnHeaders(survey_id, accessToken){
 	var req_url = api_url.base + api_url.survey_details + api_url.api_key;
 	console.log(req_url);
@@ -144,13 +147,12 @@ function _getColumnHeaders(survey_id, accessToken){
 			'Content-Type': 'application/json'
 		},
 		complete:function(jqXHR, textStatus){
-			console.log("hi");
 			if (textStatus != 'success' || jqXHR.responseJSON.errmsg ) {
 				alert('survey details api failed'); 
 			}
-			console.log(jqXHR.responseJSON.data);
-			data = jqXHR.responseJSON.data;
-			parse_survey_details(data); 
+			//console.log(jqXHR.responseJSON.data);
+			//data = jqXHR.responseJSON.data;
+			parse_survey_details(jqXHR.responseJSON.data); 
         }
 
 	});
@@ -189,24 +191,24 @@ function parse_survey_details(data){
 					//essay/comment style: 
 					headers.push(question.heading); 
 					id_to_question_name[question.question_id] = question.heading;
+					fieldTypes.push('string');
 				} else {
 					for  (k = 0; k < question.answers.length; k++){
 						answer = question.answers[k];
 						var header = question.heading + ' - ' + answer.text; 
 						headers.push(header); 
 						id_to_question_name[answer.answer_id] = header; 
+						fieldTypes.push('string');
 					}
 				}
-				fieldTypes.push('string');
 			} else if ( type === 'datetime'){
-				headers.push(question.heading);
-				fieldTypes.push('string');
-				id_to_question_name[question.question_id] = question.heading; 
 				for  (k = 0; k < question.answers.length; k++){
 					answer = question.answers[k];
-					id_to_answer_name[answer.answer_id] = answer.text;
+					var header = question.heading + ' - ' + answer.text.trim(); 
+					headers.push(header); 
+					id_to_question_name[answer.answer_id] = header; 
+					fieldTypes.push('datetime');
 				}
-				fieldTypes.push('datetime');
 			} else if (type === 'demographic'){
 				//do a if text when parsing
 				for  (k = 0; k < question.answers.length; k++){
@@ -214,8 +216,9 @@ function parse_survey_details(data){
 					var header = question.heading + ' - ' + answer.text.trim(); 
 					headers.push(header); 
 					id_to_question_name[answer.answer_id] = header; 
+					fieldTypes.push('string');
 				}
-				fieldTypes.push('string');
+				
 			} else if (type === 'matrix'){
 				for (k = 0; k < question.answers.length; k++){
 					answer = question.answers[k]; 
@@ -236,12 +239,114 @@ function parse_survey_details(data){
 
 		}
 	}
-	console.log(headers); 
-	console.log(fieldTypes);
-	console.log(id_to_question_name); 
-	console.log(id_to_answer_name);
-}
+	var transfer_data = []; 
+	transfer_data.push(tableau.connectionData); 
+	transfer_data.push(tableau.id_to_question_name);
+	transfer_data.push(tableau.id_to_answer_name); 
+	tableau.connectionData = JSON.stringify(transfer_data);
+	tableau.headersCallback(headers, fieldTypes);
+	// h = headers;
+	// f = fieldTypes;
+	// console.log(headers.length); 
+	// console.log(fieldTypes.length);
+	// console.log(id_to_question_name); 
+	// console.log(id_to_answer_name);
+};
 
+function _getData(survey_id, accessToken){
+	_getRespondentList(survey_id, accessToken);
+};
+
+function _getRespondentList(survey_id, accessToken){
+	var respondent_ids = []; 
+	var req_url = api_url.base + api_url.respondent_list + api_url.api_key;
+	var respondents;
+	var xhr = $.ajax({
+		url: req_url,
+		type: 'POST',
+		data: '{"survey_id": "' + survey_id + '"}',
+		dataType: 'json',
+		contentType: 'application/json',
+		headers: {
+			'Authorization': 'bearer ' + accessToken,
+			'Content-Type': 'application/json'
+		},
+		complete:function(jqXHR, textStatus){
+			if (textStatus != 'success' || jqXHR.responseJSON.errmsg ) {
+				alert('respondents api failed'); 
+			}
+			//alert("respondent list api worked!");
+			data = jqXHR.responseJSON.data;
+			respondents = jqXHR.responseJSON.data.respondents; 
+			for (i = 0; i < respondents.length; i++){
+				respondent_ids.push(respondents[i].respondent_id); 
+			}
+			//console.log(JSON.stringify(respondent_ids));
+			//console.log(respondents.length)
+			setTimeout(_getResponses, 1000, survey_id, accessToken, respondent_ids, 0, 2, []);
+			//_getResponses(survey_id, accessToken, respondent_ids);
+        }
+	});
+};
+
+function _getResponses(survey_id, accessToken, respondents, index, api_limit, data_array){
+	var respondents_to_query = []; 
+	var limit; 
+	if ((index+1)*100 > respondents.length){
+		limit = respondents.length; 
+	} else {
+		limit = (index+1)*100; 
+	}
+	for (var i = index*100; i < limit; i++){
+		respondents_to_query.push(respondents[i]); 
+	}
+	var request_body = {
+		"respondent_ids": respondents_to_query,
+		"survey_id": survey_id
+	};
+	//console.log(JSON.stringify(request_body));
+	var req_url = api_url.base + api_url.responses + api_url.api_key;
+	console.log(req_url);
+	var xhr = $.ajax({
+		url: req_url,
+		type: 'POST',
+		data: JSON.stringify(request_body),
+		dataType: 'json',
+		contentType: 'application/json',
+		headers: {
+			'Authorization': 'bearer ' + accessToken,
+			'Content-Type': 'application/json'
+		},
+		complete:function(jqXHR, textStatus){
+			if (textStatus != 'success' || jqXHR.responseJSON.errmsg ) {
+				console.log(textStatus);
+				console.log(jqXHR.responseJSON.errmsg);
+				alert('reponses api failed'); 
+			}
+			data_array.push(jqXHR.responseJSON.data);
+			if (limit === respondents.length){
+				parse_responses(data_array); 
+			} else if (index % api_limit === 1){
+				setTimeout(_getResponses, 1000, survey_id, accessToken, respondents, index+1, api_limit, data_array);
+			} else {
+				_getResponses(survey_id, accessToken, respondents, index+1, api_limit, data_array);
+			}
+        }
+	});
+};
+
+function parse_responses(data_array){
+	console.log(data_array[0][0].questions);
+	var data_transferred = JSON.parse(tableau.connectionData);
+	var id_to_question_name = data_transferred[1]; 
+	var id_to_answer_name = data_transferred[2];
+	console.log("parsing responses");
+	data = data_array; 
+	for (i = 0; i < data_array.length; i++){
+		console.log(1);
+	}
+	tableau.dataCallback([], '0', false);
+};
 
 
 //-------------------------------------------------- //
@@ -250,46 +355,61 @@ function parse_survey_details(data){
 var myConnector = tableau.makeConnector();
 
 myConnector.init = function() {
+	console.log('inside init');
 	var accessToken = Cookies.get("accessToken");
 
 	var hasAuth = (accessToken && accessToken.length > 0) || tableau.password.length > 0;
-	hasAuth = null;
-	update_Auth_displays(hasAuth); 
 
 	if (tableau.phase == tableau.phaseEnum.interactivePhase || tableau.phase == tableau.phaseEnum.authPhase) {
 		if (hasAuth) {	
-			tableau.initCallback();
 			tableau.password = accessToken;
-
-			return;
 		}
 	} else {
 		if (!hasAuth) {
 			tableau.abortWithError("Don't have an access token. Giving up");
 		}
 	}
+	update_Auth_displays(hasAuth); 
 
 	tableau.initCallback();
 };
 
 
 myConnector.getColumnHeaders = function() {
-	var fieldNames = ['activityType', 'activityID', 'activityCaloriesBurned', 'activityDistance', 'activityDuration', 'activityStart', 'activityEnd', 'activityHRAvg', 'activityHRPeak', 'activityHRLow', 'activityHREnding'];
-    var fieldTypes = ['string', 'string', 'int', 'int', 'datetime', 'datetime', 'datetime', 'int', 'int', 'int', 'int'];	
-	
-  tableau.headersCallback(fieldNames, fieldTypes);
+	console.log("inside getColumn");
+	var accessToken = tableau.password; 
+	var survey_id = tableau.connectionData;
+	_getColumnHeaders(survey_id, accessToken);
+
+  	//tableau.headersCallback(fieldNames, fieldTypes);
+  	// var fieldNames = ['Ticker', 'Day', 'Close'];
+   //  var fieldTypes = ['string', 'string', 'string'];
+   //  tableau.headersCallback(fieldNames, fieldTypes);
 };
 
 
 myConnector.getTableData = function(lastRecordToken) {
-	if (lastRecordToken && lastRecordToken.length > 0) {
-		requestMSHProfileByUrl(lastRecordToken);
-	} else {
-	    var accessToken = tableau.password;
-	    requestMSHProfile(accessToken);	
-	}
+	var accessToken = tableau.password;
+	var data_transferred = JSON.parse(tableau.connectionData);
+	var survey_id = data_transferred[0]; 
+	console.log(survey_id);
+	setTimeout(_getRespondentList, 1000, survey_id, accessToken);
+	//_getRespondentList(survey_id, accessToken);
+
+	// var entry = {'Do you have any other comments, questions, or concerns?': 'HELL YES'
+	// 			};
+	// var datatoreturn = []; 
+	// datatoreturn.push(entry);
+	// tableau.dataCallback(datatoreturn, datatoreturn.length.toString(), false);
+	// if (lastRecordToken && lastRecordToken.length > 0) {
+	// 	requestMSHProfileByUrl(lastRecordToken);
+	// } else {
+	//     var accessToken = tableau.password;
+	//     requestMSHProfile(accessToken);	
+	// }
 };
 
 
 tableau.registerConnector(myConnector);
 
+})();
